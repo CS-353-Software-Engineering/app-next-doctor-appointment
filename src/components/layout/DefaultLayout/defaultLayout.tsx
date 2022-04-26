f7264286a1106fed19e6285a2d059d2a5b62d9f6
Habib Shahzad <60187677+hs05888@users.noreply.github.com>
2022-04-26 23:45:31 +0500

    [FEAT] logout functionality

--- src/components/layout/DefaultLayout/defaultLayout.tsx
+++ src/components/layout/DefaultLayout/defaultLayout.tsx
@@ -3,6 +3,8 @@ import Header, { HeaderProps } from "../../core/Header";
 import Footer, { FooterProps } from "../../core/Footer";
 import { Col, Container, Row } from "react-bootstrap";
 import styles from "./styles.module.scss";
+import AuthContext from "../../../contexts/shared/auth/authContext";
+import { useContext, useEffect } from "react";
 
 type DefaultLayoutDataProps = {
   headerProps: HeaderProps;
@@ -19,12 +21,22 @@ export type DefaultLayoutProps = {
 const DefaultLayout = (props: DefaultLayoutProps) => {
   const { children } = props;
 
+  const { user } = useContext(AuthContext);
+
+  const headerProps = { user };
+
+  useEffect(() => {
+
+  }, [user])
+
+
+
   return (
     <>
       <Container fluid className="bg-color">
         <Row id="main-container">
           <Col className="p-0 m-0" id={styles["page-content-wrapper"]}>
-            {<Header />}
+            {<Header {...headerProps} />}
             <main id="main-content">{children}</main>
             <Footer />
           </Col>
